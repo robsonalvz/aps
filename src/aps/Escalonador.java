84d2830cd94eb0bd71a0e4544a6e006e35905473
ClaudioVic <claudio.victor@dce.ufpb.br>
2019-03-28 16:21:11 -0300

    Casos de Testes e alguns métodos
    todos os casos de testes a serem implementados e alguns métodos para serem desenvolvidos para testar o Escalonador

--- src/aps/Escalonador.java
+++ src/aps/Escalonador.java
@@ -1,9 +1,26 @@
-
 package aps;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Escalonador {
-
 	
-
+	
+	public Escalonador() {
+	}
+	
+	public void adicionarProcesso() {
+		
+	}
+	
+	public void finalizarProcesso() {
+		
+	}
+	
+	public void Tick(){
+		
+	}
+	
+	
 }
 
