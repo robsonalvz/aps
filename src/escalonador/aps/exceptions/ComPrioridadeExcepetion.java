bd4ae51238e45a88e04b87fefa97b233d4153f08
robsonalvz <robson.alves@dce.ufpb.br>
2019-04-10 23:51:05 -0300

    Exception adicionada

---
+++ src/escalonador/aps/exceptions/ComPrioridadeExcepetion.java
@@ -0,0 +1,14 @@
+package escalonador.aps.exceptions;
+
+public class ComPrioridadeExcepetion extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ComPrioridadeExcepetion(String msg){
+		super(msg);
+	}
+
+}
