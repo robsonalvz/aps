6acfd6daca93f8a0171a5566bd1ae89348adc298
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-04-10 22:42:04 -0300

    Teste 22
    O teste 22 consistem em o processo finalizar enquanto estava esperando, e o outro processo assumir a CPU, com prioridades.

--- src/escalonador/aps/testes/EscalonadorTest.java
+++ src/escalonador/aps/testes/EscalonadorTest.java
@@ -547,7 +547,31 @@ public class EscalonadorTest {
 
 		assertEquals(escalonador.getStatus(), resultado);
 	}
+	
+	/**
+	 * Teste 22 Com concorrencia o processo finaliza quando estava esperando. E o
+	 * primeiro processo nao perde a CPU, com prioridade;
+	 */
+	
+	@Test
+	public void comConcorrenciaProcessoFinalizaEsperandoComPrioridade() {
+		Processo p1 = new Processo("P1",0,1);
+		p1.setStatus(Status.Executando);
+		Processo p2 = new Processo("P2",0,1);
+		p2.setStatus(Status.Esperando);
 
+		escalonador.adicionarProcessoComPrioridade(p1);
+		escalonador.adicionarProcessoComPrioridade(p2);
+		estourarQuantum(escalonador.getQuantum());
+		escalonador.finalizarProcesso(p2);
+		estourarQuantum(escalonador.getQuantum());
+
+		String resultado = "P1: Executando, Tick: 0, Quantum: 2\n" + "P2: Esperando, Tick: 0, Quantum: 2\n"
+				+ "P1: Executando, Tick: 1, Quantum: 2\n" + "P2: Esperando, Tick: 1, Quantum: 2\n"
+				+ "P1: Executando, Tick: 2, Quantum: 2\n" + "P1: Executando, Tick: 3, Quantum: 2\n";
+
+		assertEquals(escalonador.getStatus(), resultado);
+	}
 
 	// Teste 28 A partir de T 16 3 Ticks
 	
@@ -569,5 +593,7 @@ public class EscalonadorTest {
 				+ "P1: Esperando, Tick: 2, Quantum: 2\n" +"P2: Executando, Tick: 2, Quantum: 2\n";
 		assertEquals(escalonador.getStatus(), resultado);
 	}
+	
+
 
 }
