88a17011ff3cdabefcd9861515487971d22501be
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-04-10 23:25:55 -0300

    Teste 27
    O teste bloqueandoProcessoEMudandoAOrdemComPrioridade() consiste em bloquear os tres processos e retorna - los na ordem desejada.

--- src/escalonador/aps/testes/EscalonadorTest.java
+++ src/escalonador/aps/testes/EscalonadorTest.java
@@ -705,7 +705,46 @@ public class EscalonadorTest {
 		assertEquals(escalonador.getStatus(), resultado);
 	}
 	
+	/**
+	 * Teste 27 bloqueia os processos e retorna na ordem P2,P1,P3
+	 */
 	
+	@Test
+	public void bloqueandoProcessoEMudandoAOrdemComPrioridade() {
+
+		Processo p1 = new Processo("P1", 0, 1);
+		escalonador.adicionarProcessoComPrioridade(p1);;
+		Processo p2 = new Processo("P2", 0, 2);
+		escalonador.adicionarProcessoComPrioridade(p2);;
+		Processo p3 = new Processo("P3", 0, 3);
+		escalonador.adicionarProcessoComPrioridade(p3);;
+
+		escalonador.ordenaPorPrioridade();
+		escalonador.tick();
+
+		escalonador.bloqueiaProcesso(p1);
+
+		escalonador.tick();
+
+		escalonador.desbloquearProcesso(p1);
+
+		escalonador.tick();
+
+		escalonador.mudarStatus();
+
+		estourarQuantum(escalonador.getQuantum());
+
+		String resultado = "P1: Executando, Tick: 0, Quantum: 2\n" + "P2: Esperando, Tick: 0, Quantum: 2\n"
+				+ "P3: Esperando, Tick: 0, Quantum: 2\n" + "P1: Bloqueado, Tick: 1, Quantum: 2\n"
+				+ "P2: Executando, Tick: 1, Quantum: 2\n" + "P3: Esperando, Tick: 1, Quantum: 2\n"
+				+ "P1: Esperando, Tick: 2, Quantum: 2\n" + "P2: Executando, Tick: 2, Quantum: 2\n"
+				+ "P3: Esperando, Tick: 2, Quantum: 2\n" + "P1: Esperando, Tick: 3, Quantum: 2\n"
+				+ "P2: Esperando, Tick: 3, Quantum: 2\n" + "P3: Executando, Tick: 3, Quantum: 2\n"
+				+ "P1: Esperando, Tick: 4, Quantum: 2\n" + "P2: Esperando, Tick: 4, Quantum: 2\n"
+				+ "P3: Executando, Tick: 4, Quantum: 2\n";
+
+		assertEquals(escalonador.getStatus(), resultado);
+	}
 
 	// Teste 28 A partir de T 16 3 Ticks
 	
