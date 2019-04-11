9402a20d6abb9e3d5387ad053f1ad9c4e4806e59
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-04-10 23:13:39 -0300

    Teste processoExecutandoBloqueadoComPrioridade()
    Esse teste consistem em o processo está executando e ser bloqueado, e utilizando prioridade.

--- src/escalonador/aps/testes/EscalonadorTest.java
+++ src/escalonador/aps/testes/EscalonadorTest.java
@@ -630,6 +630,37 @@ public class EscalonadorTest {
 		
 		assertEquals(escalonador.getStatus(), resultado);
 	}
+	
+	@Test
+	public void processoExecutandoBloqueadoComPrioridade() {
+		Processo p1 = new Processo("P1", 0, 1);
+		escalonador.adicionarProcessoComPrioridade(p1);
+		Processo p2 = new Processo("P2", 0, 2);
+		escalonador.adicionarProcessoComPrioridade(p2);
+		Processo p3 = new Processo("P3", 0, 3);
+		escalonador.adicionarProcessoComPrioridade(p3);
+
+		escalonador.ordenaPorPrioridade();
+		estourarQuantum(escalonador.getQuantum());
+
+		escalonador.bloqueiaProcesso(p1);
+
+		estourarQuantum(escalonador.getQuantum());
+
+		estourarQuantum(escalonador.getQuantum());
+
+		String resultado = "P1: Executando, Tick: 0, Quantum: 2\n" + "P2: Esperando, Tick: 0, Quantum: 2\n"
+				+ "P3: Esperando, Tick: 0, Quantum: 2\n" + "P1: Executando, Tick: 1, Quantum: 2\n"
+				+ "P2: Esperando, Tick: 1, Quantum: 2\n" + "P3: Esperando, Tick: 1, Quantum: 2\n"
+				+ "P1: Bloqueado, Tick: 2, Quantum: 2\n" + "P2: Executando, Tick: 2, Quantum: 2\n"
+				+ "P3: Esperando, Tick: 2, Quantum: 2\n" + "P1: Bloqueado, Tick: 3, Quantum: 2\n"
+				+ "P2: Executando, Tick: 3, Quantum: 2\n" + "P3: Esperando, Tick: 3, Quantum: 2\n"
+				+ "P1: Bloqueado, Tick: 4, Quantum: 2\n" + "P2: Esperando, Tick: 4, Quantum: 2\n"
+				+ "P3: Executando, Tick: 4, Quantum: 2\n" + "P1: Bloqueado, Tick: 5, Quantum: 2\n"
+				+ "P2: Esperando, Tick: 5, Quantum: 2\n" + "P3: Executando, Tick: 5, Quantum: 2\n";
+
+		assertEquals(escalonador.getStatus(), resultado);
+	}
 
 	// Teste 28 A partir de T 16 3 Ticks
 	
