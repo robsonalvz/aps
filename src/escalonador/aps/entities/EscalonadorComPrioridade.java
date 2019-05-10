5e60a1cc7c28fcfa89bb1a1d5388d2afacba8d85
ruaanc <31658602+ruaanc@users.noreply.github.com>
2019-05-09 21:45:29 -0300

    Update EscalonadorComPrioridade.java

--- src/escalonador/aps/entities/EscalonadorComPrioridade.java
+++ src/escalonador/aps/entities/EscalonadorComPrioridade.java
@@ -14,30 +14,5 @@ public class EscalonadorComPrioridade {
 	
 	
 
-	public void adicionarProcessoComPrioridade(Processo processo) throws SemPrioridadeException {
-		
-		if (processo.getPrioridade()==-1) {
-				throw new SemPrioridadeException("Processo adicionado está sem prioridade.");
-			}
-		
-		this.processos.add(processo);
-	}
-	
-	public void ordenaPorPrioridade() {
-		int cont = 0;
-		List<Processo> lista = new ArrayList<Processo>();	
-		while(cont < this.processos.size()){
-			Processo po = this.processos.get(0);
-			for(Processo p : this.processos) {
-				if(p.getPrioridade() < po.getPrioridade()) {
-					po = p;
-				}
-			}
-			lista.add(po);
-			this.processos.remove(po);
-			cont += 1;		
-		}
-		lista.add(processos.get(0));
-		this.processos = lista;
-	}
+
 }
